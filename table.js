c4b3a9a04a8e97dd1b5422275bb38ecb2a434ba6
chris jones <cjonesrun@gmail.com>
2016-12-07 19:36:05 -0500

    split js

--- table.js
+++ table.js
@@ -1,15 +1,16 @@
 startIncline = 1;
-  endIncline = 10;
-  table="<table border='1' width='100%'><tr><th bgcolor='#ffffff'>kph</th><th>pace</th>";
-  for (incline = startIncline; incline<endIncline+1; incline++)
-    table += "<th>" + incline + "%</th>";
-  table+="</tr>"
-for (speed = 800; speed < 2001; speed+=20)
-	{
-    	iSpeed = speed/100;
-      iPace = toPace( eval(60/iSpeed));
+endIncline = 10;
+table="<table border='1' width='100%'><tr><th bgcolor='#ffffff'>kph</th><th>pace</th>";
+
+for (incline = startIncline; incline<endIncline+1; incline++)
+	table += "<th>" + incline + "%</th>";
+table+="</tr>"
+
+for (speed = 800; speed < 2001; speed+=20) {
+	iSpeed = speed/100;
+	iPace = toPace( eval(60/iSpeed));
     	table += "<tr><td>" + iSpeed + "</td><td bgcolor=\"" + getBGColour(iPace) + "\">" + iPace + "</td>";
-      for (incline = startIncline; incline<endIncline+1; incline++)  
+	for (incline = startIncline; incline<endIncline+1; incline++)  
       {
       	eSpeed = iSpeed+iSpeed*(incline)*9/200;
       	eSpeed = Math.round(accuracy*eSpeed)/accuracy;
